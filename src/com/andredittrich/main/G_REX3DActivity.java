294c98439e07d5d77f289ec8226c496310f036bd
andre <andre@andre-gik-laptop.(none)>
2012-04-12 16:23:05 +0200

    test commit

--- src/com/andredittrich/main/G_REX3DActivity.java
+++ src/com/andredittrich/main/G_REX3DActivity.java
@@ -2,12 +2,16 @@ package com.andredittrich.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class G_REX3DActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView textview = new TextView(this);
+        textview.setText("TEST");
+        
+        setContentView(textview);
     }
 }
