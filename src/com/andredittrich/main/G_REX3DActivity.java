67fe5979c48c3e5fd1c9e17eed4942e19e3a9afd
andre <andre@andre-gik-laptop.(none)>
2012-04-12 15:39:20 +0200

    initial commit !

---
+++ src/com/andredittrich/main/G_REX3DActivity.java
@@ -0,0 +1,13 @@
+package com.andredittrich.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class G_REX3DActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
